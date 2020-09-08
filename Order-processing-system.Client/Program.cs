982840ce3b3e6b2ae34fdece01224771219b1bb9
sumbagaara <sumbagaara@gmail.com>
2020-09-08 23:20:58 +0530

    Client app to showcase how classes can be consumed

---
+++ Order-processing-system.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Order_processing_system.Domain.Concrete_Implementation;
+using System;
+
+namespace Order_processing_system.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Physical Product Use Case
+            var PhysicalProductPayment = new PhysicalProduct();
+            Console.WriteLine(PhysicalProductPayment.ApplyRules());
+
+            Console.ReadKey();
+
+        }
+    }
+}
